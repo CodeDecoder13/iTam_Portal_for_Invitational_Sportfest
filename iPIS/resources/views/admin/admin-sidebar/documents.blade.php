ec302568b64f86d60867a650cbd1ebaa1237052c
Rhuzzel Paramio <87302705+CodeDecoder13@users.noreply.github.com>
2024-08-03 17:26:15 +0800

    Update documents.blade.php

--- iPIS/resources/views/admin/admin-sidebar/documents.blade.php
+++ iPIS/resources/views/admin/admin-sidebar/documents.blade.php
@@ -1,10 +1,16 @@
 <x-app-layout>
+    <section class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        
+    </section>
 
-        @section('content')
-            <div class="container mx-auto">
-                <h1 class="text-2xl font-bold">My Documents</h1>
-                <p>Manage your documents here.</p>
-            </div>
-        @endsection
 
+    <section class="grid grid-cols-1">
+        <h1 class="font-bold mb-2 text-3xl">Document Requirements</h1>
+        <h3>Manage and Organize Team Documents</h3>
+        <div class="grid grid-cols-1 mt-5">
+            
+            
+
+        </div>
+    </section>
 </x-app-layout>
