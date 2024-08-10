42a4201088496c57905b701df7d78ade30d5b111
Rhuzzel Paramio <87302705+CodeDecoder13@users.noreply.github.com>
2024-08-10 10:20:25 +0800

    Update dashboard.blade.php

--- iPIS/resources/views/dashboard.blade.php
+++ iPIS/resources/views/dashboard.blade.php
@@ -82,17 +82,17 @@
             <h1 class="text-3xl font-bold mb-4">Dashboard</h1>
             <h3 class="text-xl mb-8">Welcome, Coach <span class="underline">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</span></h3>
             <div class="flex flex-col items-center justify-center min-h-screen">
-                <div class="text-center">
-                    <div class="mb-6">
+                <div class="text-center justify-center">
+                    <div class="mb-6 justify-center">
                         <img src="{{ asset('/images/search-magnifying.png') }}" alt="Approval Icon" class="w-24 h-24 mx-auto">
                     </div>
-                    <h1 class="text-xl font-semibold mb-4">Your account is temporarily on hold for approval by our admins.</h1>
-                    <p class="text-gray-600 mb-8">Please allow 24-48 hours for the approval process. We appreciate your patience.</p>
-
+                    <h1 class="text-xl font-semibold mb-4 justify-center">Your account is temporarily on hold for approval by our admins.</h1>
+                    <p class="text-gray-600 mb-8 justify-center">Please allow 24-48 hours for the approval process. We appreciate your patience.</p>
+            
                     <!-- Logout Button -->
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button type="submit" class="px-4 py-2 bg-green-700 text-white rounded-full hover:bg-green-800 focus:outline-none">
+                        <button type="submit" class="px-4 py-2 bg-green-700 text-white rounded-full hover:bg-green-800 focus:outline-none justify-center">
                             Logout
                         </button>
                     </form>
