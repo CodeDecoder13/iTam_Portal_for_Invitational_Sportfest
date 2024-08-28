778359279cf68210ea272ee931d29834f7bd57b5
Rhuzzel Paramio <87302705+CodeDecoder13@users.noreply.github.com>
2024-08-28 15:30:57 +0800

    Update coach-approval.blade.php

--- iPIS/resources/views/admin/admin-sidebar/coach-approval.blade.php
+++ iPIS/resources/views/admin/admin-sidebar/coach-approval.blade.php
@@ -316,7 +316,9 @@
                     </div>
                 </div>
             </div>
-
+            <!-- new layout -->
+            
+              
 
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.x.x/dist/js/bootstrap.bundle.min.js"></script>
