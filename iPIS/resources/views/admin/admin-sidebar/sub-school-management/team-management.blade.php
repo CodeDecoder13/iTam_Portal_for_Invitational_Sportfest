83b1ad7f5e7d078dad5535197a9a98e5c6310f31
Rhuzzel Paramio <87302705+CodeDecoder13@users.noreply.github.com>
2024-10-11 17:54:31 +0800

    minor revisions

--- iPIS/resources/views/admin/admin-sidebar/sub-school-management/team-management.blade.php
+++ iPIS/resources/views/admin/admin-sidebar/sub-school-management/team-management.blade.php
@@ -53,7 +53,7 @@
     </section>
     
 
-    <!-- Add Team Modal -->
+    
     <!-- Add Team Modal -->
 <div id="addTeamModal" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full hidden">
     <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
